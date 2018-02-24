e6e049489cb8684c0ec1d3f7fe06bb5aa213c7b0
Dominik Gorecki <dom@nikgo.com>
2018-02-23 21:54:19 -0500

    Added simple struct study and unit tests

---
+++ UnitTests/StructStudyTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StructStudy;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StructStudyTest
+    {
+
+        [TestMethod]
+        public void StructValueType()
+        {
+            var latLongStructType = new LatLongStruct()
+            {
+                Latitude = 1,
+                Longitude = 1
+            };
+
+            var latLongStructTypeModed = StructStudyHelpers.ModStruct(latLongStructType);
+
+            Assert.AreNotEqual(latLongStructType, latLongStructTypeModed);
+        }
+
+        [TestMethod]
+        public void StructRefType()
+        {
+            var latLongClass = new LatLongClass()
+            {
+                Latitude = 1,
+                Longitude = 1
+            };
+
+            var latLongClassModed = StructStudyHelpers.ModClass(latLongClass);
+
+            Assert.AreSame(latLongClass, latLongClassModed);
+        }
+
+    }
+}
---
+++ cSharpExamples/StructStudy.cs
@@ -0,0 +1,32 @@
+﻿namespace StructStudy 
+{
+    public static class StructStudyHelpers
+    {
+        public static LatLongStruct ModStruct(LatLongStruct llStruct)
+        {
+            llStruct.Latitude *= (decimal)1.1;
+            llStruct.Longitude *= (decimal)1.1;
+            return llStruct;
+        }
+
+        public static LatLongClass ModClass(LatLongClass llClass)
+        {
+            llClass.Longitude *= (decimal)1.1;
+            llClass.Latitude *= (decimal)1.1;
+            return llClass;
+        }
+
+    }
+
+    public struct LatLongStruct
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    public class LatLongClass
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
